53bed1ae34f282c0e439eebf2aa3895124997220
Liron <tnd.art@gmail.com>
2017-11-06 22:18:20 +0200

    youtube feature - got data

--- ShauliBlog/DAL/BlogInitializer.cs
+++ ShauliBlog/DAL/BlogInitializer.cs
@@ -19,30 +19,37 @@ namespace ShauliBlog.DAL {
             var fans = new List<Fan> {
                 new Fan(1, "Liron", "Elbaz", new DateTime(1992,5,9), Gender.Male, 9, "liron", "123456", false),
                 new Fan(2, "Yakir", "Nadav", new DateTime(1992,8,23), Gender.Male, 5, "yakir", "123456", false),
-                new Fan(3, "Mike", "Laury", new DateTime(1980,8,2), Gender.Male, 7, "admin", "admin", true),
+                new Fan(3, "Shem-tova", "Yerhi", new DateTime(1992,8,2), Gender.Female, 7, "admin", "admin", true),
+                new Fan(4, "Vered", "Gotliv", new DateTime(1980,6,8), Gender.Female, 19, "vered", "123456", true)
             };
 
             var comments = new List<Comment> {
                 new Comment(1, "Comment 1", "Liron", "https://Liron.com", DateTime.Today, "first comment ever", 1),
                 new Comment(2, "Comment 2", "Yakir", "https://Yakir.com", DateTime.Today, "Second comment ever", 1),
-                new Comment(3, "Comment 3", "Igor", "https://Igor.com", DateTime.Today, "Third comment ever", 2),
+                new Comment(3, "Comment 3", "Shemtova", "https://Shemtova.com", DateTime.Today, "Third comment ever", 2),
                 new Comment(4, "Comment 4", "Yakir", "https://Yakir.com", DateTime.Today, "Fourth comment ever", 3),
-                new Comment(5, "Comment 5", "Igor", "https://Igor.com", DateTime.Today, "fifth comment ever", 4),
-                new Comment(6, "Comment 6", "Igor", "https://Igor.com", DateTime.Today, "sixth comment ever", 5)
+                new Comment(5, "Comment 5", "Liron", "https://Liron.com", DateTime.Today, "fifth comment ever", 4),
+                new Comment(6, "Comment 6", "Shemtova", "https://Shemtova.com", DateTime.Today, "sixth comment ever", 5),
+                new Comment(7, "Comment 7", "Vered", "https://Vered.com", DateTime.Today, "7 comment ever", 6),
+                new Comment(8, "Comment 8", "Vered", "https://Vered.com", DateTime.Today, "8 comment ever", 6),
+                new Comment(9, "Comment 9", "Yakir", "https://Yakir.com", DateTime.Today, "9 comment ever", 7)
 
             };
 
             var posts = new List<Post> {
-                new Post(1, "Post 1", "Liron", "https://Liron.com", DateTime.Today, "First post ever", "images/flower.png", "", comments.FindAll(x => x.ID < 3), new List<Tag> { tags[0], tags[1] }),
+                new Post(1, "Post 1", "Shemtova", "https://Shemtova.com", DateTime.Today, "First post ever", "images/flower.png", "", comments.FindAll(x => x.ID < 3), new List<Tag> { tags[0], tags[1] }),
                 new Post(2, "Post 2", "Yakir", "https://Yakir.com", DateTime.Today.AddDays(-2), "Second post ever", "", "", comments.FindAll(x => x.ID == 3), new List<Tag> { tags[0], tags[2] }),
-                new Post(3, "Post 3", "Yakir", "https://Yakir.com", DateTime.Today.AddDays(-3), "third post ever", "", "", comments.FindAll(x => x.ID == 4), new List<Tag> { tags[0] }),
+                new Post(3, "Post 3", "Vered", "https://Vered.com", DateTime.Today.AddDays(-3), "third post ever", "", "", comments.FindAll(x => x.ID == 4), new List<Tag> { tags[0] }),
                 new Post(4, "Post 4", "Liron", "https://Yakir.com", DateTime.Today.AddDays(-3), "forth post ever", "", "", comments.FindAll(x => x.ID == 5), new List<Tag> { tags[1] }),
-                new Post(5, "Post 5", "Yakir", "https://Yakir.com", DateTime.Today.AddDays(-3), "fifth post ever", "", "", comments.FindAll(x => x.ID == 6), tags)
+                new Post(5, "Post 5", "Yakir", "https://Yakir.com", DateTime.Today.AddDays(-8), "fifth post ever", "", "", comments.FindAll(x => x.ID == 6), new List<Tag> { tags[0], tags[2] }),
+                new Post(6, "Post 6", "Vered", "https://Vered.com", DateTime.Today.AddDays(-9), "6 post ever", "", "", comments.FindAll(x => x.ID == 7 || x.ID == 8), tags),
+                new Post(7, "Post 7", "Shemtova", "https://Shemtova.com", DateTime.Today.AddDays(-29), "7 post ever", "", "", comments.FindAll(x => x.ID == 9), new List<Tag> { tags[2] })
             };
 
-            tags[0].Posts = new List<Post> { posts[0], posts[1], posts[2], posts[4] };
+
+            tags[0].Posts = new List<Post> { posts[0], posts[1], posts[2], posts[4], posts[5] };
             tags[1].Posts = new List<Post> { posts[0], posts[3], posts[4] };
-            tags[2].Posts = new List<Post> { posts[1], posts[4] };
+            tags[2].Posts = new List<Post> { posts[1], posts[4], posts[5], posts[6] };
 
             fans.ForEach(f => context.Fans.Add(f));
             context.SaveChanges();
